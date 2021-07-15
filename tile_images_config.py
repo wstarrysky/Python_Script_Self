247968e53bb83b74f450fd7d11c9b1ae3fbedad1
starrysky <wenbo_jiang@163.com>
2021-07-15 14:00:16 +0800

    complete

--- tile_images_config.py
+++ tile_images_config.py
@@ -7,6 +7,7 @@ import os
 import argparse
 import traceback
 import shutil
+from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -19,7 +20,7 @@ def read_image():
     img_list = []
     for file in file_list:
         if file.split(".")[-1] in format_list:
-            img_list.append(file)
+            img_list.append(os.path.join(args.src_path, file))
     return img_list
 
 
@@ -34,7 +35,7 @@ def showImages(x):
     for file in x:
         count += 1
         plt.subplot(args.row, args.col, count)
-        plt.imshow(cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2RGB))
+        plt.imshow(cv2.cvtColor(cv2.imdecode(np.fromfile(file, dtype=np.uint8), 1), cv2.COLOR_BGR2RGB))
         plt.xticks([])
         plt.yticks([])
     plt.show()
@@ -54,7 +55,7 @@ if __name__ == "__main__":
     try:
         images_list = read_image()
         for image in images_list:
-            img = cv2.imread(image)  # 变为Rgb图片
+            img = cv2.imdecode(np.fromfile(image, dtype=np.uint8), 1)  # 变为Rgb图片
             """np.vstack()按垂直方向（行顺序）堆叠数组构成一个新的数组
             堆叠的数组需要具有相同的维度
             
@@ -67,6 +68,8 @@ if __name__ == "__main__":
             size = min(w // args.col, h // args.row)
             preShowList = []
             pre_show = np.array([])
+            if "./" in image:
+                image = image.replace("./", "")
             save_dir = os.path.join(args.save_path, image.split('.')[0])
             if os.path.exists(save_dir):
                 shutil.rmtree(save_dir)
@@ -76,7 +79,8 @@ if __name__ == "__main__":
                     img_name = f"{i + 1}_{k + 1}.jpg"
                     save_img = img[i * size:(i + 1) * size, k * size:(k + 1) * size, :]
                     result = f"{save_dir}/{img_name}"
-                    cv2.imwrite(result, save_img)
+                    # cv2.imwrite(result, save_img)
+                    cv2.imencode(".jpg", save_img)[1].tofile(result)  # 采用中文编码
                     preShowList.append(result)
             if args.preImage:
                 showImages(preShowList)
